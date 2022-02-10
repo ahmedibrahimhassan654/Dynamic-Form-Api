369a92e7862b8f34ebcda72afd9005b9dfe40a51
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-02-10 19:22:15 +0200

    initiate node server and add mongo db with some packadge

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+const app = express();
+//Route files
+// const bootcamps = require("./routes/bootcamps");
+
+//Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Enable CORS
+app.use(cors());
+
+// Mount routers
+// app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
